17be32e15ab3142848335a4b855f3bfe87052602
Fuad Tanvir <omi.fuad33@gmail.com>
2021-06-02 00:07:01 +0600

    header done

--- wp-content/themes/omnispace_sitara/header.php
+++ wp-content/themes/omnispace_sitara/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,27 +23,36 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'omnispace_sitara' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="o_header_container">
-			<div class="o_logo_container">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/sitara_logo.jpg" alt="logo not found">
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<header id="masthead" class="site-header">
+			<div class="o_header_container">
+				<div class="o_logo_container">
+					<div class="row">
+						<div class="col-lg-2">
+							<div class="row">
+								<div class="col-lg-2 o_header_home_icon">
+									<i class="fa fa-home"></i>
+								</div>
+								<div class="col-lg-10">
+									<p class="o_header_title">Sitara's Story</p>
+									<p class="o_header_sub_title">Into the light</p>
+								</div>
+							</div>
+						</div>
+						<div class="o_navigation_menu_container col-lg-10">
+							<nav id="site-navigation" class="main-navigation">
+								<?php
+								wp_nav_menu(
+									array(
+										'theme_location' => 'menu-1',
+										'menu_id'        => 'primary-menu',
+									)
+								);
+								?>
+							</nav>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="o_navigation_menu_container">
-
-			</div>
-		</div>
-		<nav id="site-navigation" class="main-navigation">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		</header><!-- #masthead -->
